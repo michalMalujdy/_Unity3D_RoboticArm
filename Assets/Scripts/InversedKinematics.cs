e6bd80567de91f8a58d48d8cc027323b270fffec
Dexter <natri4x@gmail.com>
2016-12-20 11:17:00 +0100

    Initial commit. Created very simple 3D model of robotic arm. So far there is a possibility to rotate every joint by its slider. Started to implement inverse kinematics. Due to that there was a need to rotate all the world around X axis, so that the base corrdinate system have Z axis up (Denavit-Hartenberg notation).

---
+++ Assets/Scripts/InversedKinematics.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InversedKinematics : MonoBehaviour {
+
+    public InputField xPositionInpuField;
+    public InputField yPositionInpuField;
+    public InputField zPositionInpuField;
+
+    public Text IK_Text;
+
+    //Desired rotations for every of the joints
+    private float theta1;
+    private float theta2;
+    private float theta3;
+
+    //Turn off text timer
+    bool isTimerOn = false;
+    float duration = 3.0f;
+    float passedTime = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isTimerOn)
+        {
+            passedTime += Time.deltaTime;
+            if(passedTime >= duration)
+            {
+                IK_Text.gameObject.SetActive(false);
+                passedTime = 0.0f;
+                isTimerOn = false;
+            }
+        }
+	}
+
+    public void CalculateInversedKinematics()
+    {
+        if (!CheckIfPointInWorkingVolume())
+        {
+            IK_Text.gameObject.SetActive(true);
+            passedTime = 0.0f;
+            isTimerOn = true;
+        }
+        Debug.Log("OK!");
+    }
+
+    bool CheckIfPointInWorkingVolume()
+    {
+        return false;
+    }
+
+    void CalculateIK()
+    {
+        float r; //temporary variable helping calculating thetas. It is a vector between the Cylinder1 and the Grabber on YX plane.
+
+        //Desired position of the Grabber
+        float px;
+        float py;
+        float pz;
+
+        //Reading desired position of the Grabber from input fields in menu
+        //px = 
+    }
+}
---
+++ Assets/Scripts/RotateBySlider.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RotateBySlider : MonoBehaviour
+{
+    public Slider slider;
+    public RotationAxis rotationAxis;
+
+    public enum RotationAxis
+    {
+        X = 1,
+        Y = 2,
+        Z = 3
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        slider.onValueChanged.AddListener(delegate { Rotate(); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Rotate()
+    {
+        if (rotationAxis == RotationAxis.X)
+        {
+            transform.localRotation = Quaternion.Euler(slider.value, 0.0f, 0.0f);
+        }
+        if (rotationAxis == RotationAxis.Y)
+        {
+            transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, slider.value, transform.localRotation.eulerAngles.z);
+        }
+        if (rotationAxis == RotationAxis.Z)
+        {
+            transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, transform.localRotation.eulerAngles.y, slider.value);
+        }
+    }
+}
