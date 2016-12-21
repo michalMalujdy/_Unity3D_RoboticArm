ec9ee888ba63e1a15a95639319f3a3f782d8619d
Dexter <natri4x@gmail.com>
2016-12-21 11:23:02 +0100

    Writed a script for inversed kinematics. It works roughly fine, but it returns theta2 in values only in I quarter instead of full range 360 degrees. Also when theta2 is very close to 0, it returns approxymately 2.46f, although when theta2 is not eqal to 0, the value is very close to the truth (difference is about 0.1 degree).

---
+++ Assets/Scripts/HeightFromGround.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeightFromGround : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        float height = transform.position.z;
+        Debug.Log(gameObject.name + ": " + height);
+	}
+}
--- Assets/Scripts/InversedKinematics.cs
+++ Assets/Scripts/InversedKinematics.cs
@@ -9,6 +9,7 @@ public class InversedKinematics : MonoBehaviour {
     public InputField zPositionInpuField;
 
     public Text IK_Text;
+    public Transform Grabber;
 
     //Desired rotations for every of the joints
     private float theta1;
@@ -46,17 +47,19 @@ public class InversedKinematics : MonoBehaviour {
             passedTime = 0.0f;
             isTimerOn = true;
         }
-        Debug.Log("OK!");
+
+        CalculateIK();
+        Debug.Log("T1=" + RadiansToDegrees(theta1) + ", T2=" + RadiansToDegrees(theta2));
     }
 
     bool CheckIfPointInWorkingVolume()
     {
-        return false;
+        return true;
     }
 
     void CalculateIK()
     {
-        float r; //temporary variable helping calculating thetas. It is a vector between the Cylinder1 and the Grabber on YX plane.
+        float r; //temporary variable helping calculating thetas. It is a length of a vector between the Cylinder1 and the Grabber on the YX plane.
 
         //Desired position of the Grabber
         float px;
@@ -64,6 +67,30 @@ public class InversedKinematics : MonoBehaviour {
         float pz;
 
         //Reading desired position of the Grabber from input fields in menu
-        //px = 
+        px = Grabber.position.x;
+        py = Grabber.position.y;
+        pz = Grabber.position.z;
+
+        float l1 = 1.95f;
+        float l2 = 1.65f;
+        float l3 = 0.8f;
+        float l4 = 0.95f + 0.348f;
+
+        r = Mathf.Sqrt(Mathf.Pow(px, 2.0f) + Mathf.Pow(py, 2.0f));
+
+        //Calculating theta2
+        float sinTheta2 = Mathf.Sqrt(Mathf.Pow(r,2.0f) - Mathf.Pow(l2 + l4, 2.0f)) / l3;
+        theta2 = Mathf.Atan2(sinTheta2, Mathf.Sqrt(1 - Mathf.Pow(sinTheta2, 2.0f)));
+        //theta2 = Mathf.Asin(sinTheta2);
+
+        //Calculating theta1
+        float beta = Mathf.Atan2(py, px);
+        float alpha = Mathf.Atan2(l3 * sinTheta2, r);
+        theta1 = beta - alpha;
+    }
+
+    float RadiansToDegrees(float angle)
+    {
+        return angle * 180.0f / Mathf.PI;
     }
 }
