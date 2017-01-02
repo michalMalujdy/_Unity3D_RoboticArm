a0c584ac4b3afe314abce56980eaf4024380880f
Dexter <natri4x@gmail.com>
2017-01-02 16:54:31 +0100

    Completed working volume. Added a toggle to show and hide it anytime.

--- Assets/Scripts/WorkingVolume.cs
+++ Assets/Scripts/WorkingVolume.cs
@@ -4,17 +4,26 @@ using System.Collections;
 public class WorkingVolume : MonoBehaviour {
 
     public float ThetaScale = 0.01f;
-    public float radius = 3f;
+    private float radiusMax = 1.65f + 0.95f + 0.348f;
     private int Size;
     private LineRenderer LineDrawer;
     private float Theta = 0f;
-    private float z = 1.5f;
+    private float z = 1.95f;
+
+
+
+    //new mode
+    public Transform cylinderTemplate;
+    private Vector3 zAxis = new Vector3(0.0f, 0.0f, 1.0f);
+    bool isWorkingVolumeShown = false;
 
     void Start()
     {
         LineDrawer = GetComponent<LineRenderer>();
 
-        DrawCircle();
+        Create();
+
+        gameObject.SetActive(false);
     }
 
     void Update()
@@ -30,9 +39,32 @@ public class WorkingVolume : MonoBehaviour {
         for (int i = 0; i < Size; i++)
         {
             Theta += (2.0f * Mathf.PI * ThetaScale);
-            float x = radius * Mathf.Cos(Theta);
-            float y = radius * Mathf.Sin(Theta);
+            float x = radiusMax * Mathf.Cos(Theta);
+            float y = radiusMax * Mathf.Sin(Theta);
             LineDrawer.SetPosition(i, new Vector3(x, y, z));
         }
+    }
+
+    void Create()
+    {
+        for (int i = 1; i <= 360; i+=2)
+        {
+            /*GameObject newCylinder = new GameObject();
+            newCylinder = cylinderTemplate.gameObject;
+            newCylinder.transform.parent = cylinderTemplate.parent;*/
+
+            Transform newCylinder = (Transform) Instantiate(cylinderTemplate);
+            newCylinder.parent = cylinderTemplate.parent;
+
+            newCylinder.Rotate(zAxis, i);
+
+            Debug.Log("a");
+        }
+    }
+
+    public void ToggleShowWorkingVolume()
+    {
+        isWorkingVolumeShown = !isWorkingVolumeShown;
+        gameObject.SetActive(isWorkingVolumeShown);
     }
 }
